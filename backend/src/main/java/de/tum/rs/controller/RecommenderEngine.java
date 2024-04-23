1910b8e1cd7f98f20089a0230d467c430ba6df52
Jingyi Jia <jingyi.jia@tum.de>
2024-04-23 17:26:26 +0200

    add Thread.sleep for delay

--- backend/src/main/java/de/tum/rs/controller/RecommenderEngine.java
+++ backend/src/main/java/de/tum/rs/controller/RecommenderEngine.java
@@ -40,7 +40,7 @@ public class RecommenderEngine {
 	public void regiserUser(String userId, List<Integer> topicIds) throws InterruptedException {
 		String url = PYTHON_SERVICE_URL + "/register?userId=" + userId;
 		String response = restTemplate.postForObject(url, topicIds, String.class);
-//		Thread.sleep(500);
+		Thread.sleep(500);
 		log.info("Called Model to register user {} with topics {}", userId, topicIds);
 		log.info("From Model: {}", response);
 	}
@@ -84,7 +84,7 @@ public class RecommenderEngine {
 			);
 			log.info("Invoking model update for User {} with {} Feedbacks", userId, recentFeedbacks.size());
 			String response = restTemplate.postForObject(url, recentFeedbacks, String.class);
-			//		Thread.sleep(500);
+			Thread.sleep(500);
 			log.info("From Model: {}", response);
 		} catch (Exception e) {
 			log.error("Error while invoking model update", e);
